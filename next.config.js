9b0a60b4e89d2457bab3aa550c2e7cd24f91b105
Amin Khayam <aminkhayam@fattureincloud.it>
2023-06-18 16:00:00 +0200

    chore(project): use proper next config in prod/local env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export'
-}
+
+
+const nextConfig = process.env.ENVIRONMENT === 'local'
+  ? {}
+  : { output: 'export' }
 
 module.exports = nextConfig
